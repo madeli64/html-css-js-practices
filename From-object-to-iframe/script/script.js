f527345c404c1c66f5222761b453ed1f87624c15
madeli64 <mohsenadeli2024@hotmail.com>
2024-09-16 13:31:16 -0400

    Add files via upload

---
+++ From-object-to-iframe/script/script.js
@@ -0,0 +1,6 @@
+// This script prevent loading iframe while loading your page
+window.onload = function() {
+    document.getElementById("myIframe").src = "https://www.youtube.com/embed/J2i0cZWCdq4?si=A1JjVyp9lUOskAWQ";
+}
+
+
